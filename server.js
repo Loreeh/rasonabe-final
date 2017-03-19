f40ca4dc845b7090acf0e2735e1b3bf9ddc25ad8
Loreeh <lauriejohn12345@gmail.com>
2017-03-19 23:46:53 +0800

    deploy to heroku

---
+++ server.js
@@ -0,0 +1,29 @@
+
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, 'movies/index.html'));
+});
+app.get('/1999', function(req, res){
+  res.sendFile(path.join(__dirname, 'movies/Part1.html'));
+});
+app.get('/2004', function(req, res){
+  res.sendFile(path.join(__dirname, 'movies/Part2.html'));
+});
+app.get('/2007', function(req, res){
+  res.sendFile(path.join(__dirname, 'movies/Part3.html'));
+});
+app.get('*', function(req, res){
+  res.status(404).sendFile(path.join(__dirname, 'views/404.html'));
+});
+
+var server = app.listen(app.get('port'), function(){
+  console.log('Server listening on port ',app.get('port'));
+});
